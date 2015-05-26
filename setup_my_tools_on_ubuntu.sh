44cfaaf915c0ac119cc9a311fe4046e577a9fe62
Kimitake Mizobe <kimimizobe@gmail.com>
2015-05-26 18:44:37 -0400

    Update setup_my_tools_on_ubuntu.sh

--- setup_my_tools_on_ubuntu.sh
+++ setup_my_tools_on_ubuntu.sh
@@ -1,4 +1,5 @@
-## basic things ##
+## execut with sudo
+## basic modules
 apt-get update
 apt-get -y install mercurial
 apt-get -y install guake
@@ -8,19 +9,14 @@ apt-get -y install bzr
 apt-get -y install build-dep
 apt-get -y install emacs24
 
-## emacs24.5 ##
-#wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
-#tar -xf emacs-24.5.tar.gz
-#cd emacs-24.5
-#./configure
-#make
-#make install
-#cd ..
-
-# = ibus setup is needed to enable mark-set
-# = The following script should be executed by each user
-# mkdir ~/.emacs.d
-# mv init.el ~/.emacs.d
+## emacs24.5
+wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
+tar -xf emacs-24.5.tar.gz
+cd emacs-24.5
+./configure
+make
+make install
+cd ..
 
 ## chrome ##
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
@@ -28,3 +24,7 @@ sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /et
 sudo apt-get update 
 sudo apt-get -y install google-chrome-stable
 
+## execute without sudo
+# mkdir ~/.emacs.d
+# mv init.el ~/.emacs.d
+# = ibus setup is needed to enable mark-set
