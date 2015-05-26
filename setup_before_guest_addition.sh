ca14e79ccf35802c62cccaa810b684cd98fb4224
Kimitake Mizobe <kimimizobe@gmail.com>
2015-05-26 12:59:39 -0400

    Change to clone setuptools repository

--- setup_before_guest_addition.sh
+++ setup_before_guest_addition.sh
@@ -2,4 +2,5 @@ sudo apt-get update
 sudo apt-get -y install build-essential
 sudo apt-get -y install linux-headers-generic
 sudo apt-get -y install git
+git clone https://github.com/belemizz/setuptools
 
