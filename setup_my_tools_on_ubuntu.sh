d89117920948a503a1936b8b0e12f611d6f829e2
Kimitake Mizobe <kimimizobe@gmail.com>
2015-05-26 15:27:57 -0400

    Update

--- setup_my_tools_on_ubuntu.sh
+++ setup_my_tools_on_ubuntu.sh
@@ -1,30 +1,26 @@
 ## basic things ##
-
 apt-get update
 apt-get -y install mercurial
-
-## terminal ##
 apt-get -y install guake
 apt-get -y install terminator
 apt-get -y install byobu
-
-## emacs ##
 apt-get -y install bzr
 apt-get -y install build-dep
 apt-get -y install emacs24
-wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
-tar -xf emacs-24.5.tar.gz
-cd emacs-24.5
-./configure
-make
-make install
-cd ..
+
+## emacs24.5 ##
+#wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
+#tar -xf emacs-24.5.tar.gz
+#cd emacs-24.5
+#./configure
+#make
+#make install
+#cd ..
 
 # = ibus setup is needed to enable mark-set
 # = The following script should be executed by each user
 # mkdir ~/.emacs.d
 # mv init.el ~/.emacs.d
-
 
 ## chrome ##
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
