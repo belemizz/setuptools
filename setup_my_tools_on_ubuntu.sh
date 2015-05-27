27d2ae44d1f3d635dd867ef3dc1d5665198b3c8a
Kimitake Mizobe <kimimizobe@gmail.com>
2015-05-27 07:02:37 -0400

    Update setup_my_tools_on_ubuntu.sh

--- setup_my_tools_on_ubuntu.sh
+++ setup_my_tools_on_ubuntu.sh
@@ -1,14 +1,20 @@
 ## execut with sudo
 ## basic modules
 apt-get update
-apt-get -y install mercurial
 apt-get -y install guake
-apt-get -y install terminator
 apt-get -y install byobu
+apt-get -y install mercurial
 apt-get -y install bzr
+
+## chrome ##
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update 
+sudo apt-get -y install google-chrome-stable
 
 ## emacs24.5
 apt-get -y build-dep emacs24
+# Need Posix setup
 wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
 tar -xf emacs-24.5.tar.gz
 cd emacs-24.5
@@ -16,12 +22,6 @@ cd emacs-24.5
 make
 make install
 cd ..
-
-## chrome ##
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update 
-sudo apt-get -y install google-chrome-stable
 
 ## execute without sudo
 # mkdir ~/.emacs.d
