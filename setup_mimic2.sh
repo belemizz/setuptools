10d11509782adc5be3a26e072179225887909322
Kimitake Mizobe <kimimizobe@gmail.com>
2015-05-27 12:10:16 -0400

    Update setup_mimic2

--- setup_mimic2.sh
+++ setup_mimic2.sh
@@ -1,18 +1,21 @@
-## exucute with sudo ##
-apt-get update
-apt-get -y install postgresql
-apt-get -y install pgadmin3
+## execute without sudo
+sudo apt-get update
+sudo apt-get -y install postgresql
+sudo apt-get -y install pgadmin3
 
 mkdir mimic_setup
 cd mimic_setup
-cp /media/sf_Vbox_share/MIMIC-Importer-2.6.tar.gz .
-tar xfvz MIMIC-Importer-2.6.tar.gz
-cd MIMIC-Importer-2.6
-cp /media/sf_Vbox_share/mimic2cdb-2.6-00.tar.gz .
-cp /media/sf_Vbox_share/mimic2cdb-2.6-Definitions.tar.gz .
-./prep.sh
-cd ..
-cd ..
 
-## execute without sudo
-# ./import.sh
+sudo cp /media/sf_Vbox_share/MIMIC-Importer-2.6.tar.gz .
+sudo cp /media/sf_Vbox_share/mimic2cdb-2.6-00.tar.gz .
+sudo cp /media/sf_Vbox_share/mimic2cdb-2.6-Definitions.tar.gz .
+sudo chown kimitake *.tar.gz
+sudo chmod 777 *.tar.gz
+
+tar xfvz MIMIC-Importer-2.6.tar.gz
+mv mimic2cdb* MIMIC-Importer-2.6
+cd MIMIC-Importer-2.6
+sudo ./prep.sh
+./import.sh
+cd ..
+cd ..
